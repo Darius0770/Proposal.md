3601e5b07e7afc2d52afabe7df18cea7cd85944f
Darius0770 <tur65426@temple.edu>
2025-03-25 15:06:55 -0400

    Add files via upload

---
+++ Proposal.md.py
@@ -0,0 +1,11 @@
+# Preproposal
+## What idea(s) do you have for your final porject?
+
+
+
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+
+
+#Do you have any questions of your own?
