bfeeda6225496b064c9699c3458be69766e98030
Darius0770 <tur65426@temple.edu>
2025-03-27 11:39:18 -0400

    Update Proposal.md.py

--- Proposal.md.py
+++ Proposal.md.py
@@ -1,11 +1,18 @@
 # Preproposal
-## What idea(s) do you have for your final porject?
+## What idea(s) do you have for your final project?
+I want to create a small video game
+I was thinking something like Pong, or Flappy Bird
+I was even thinking about a visual novel kind of game
+I also wouldn't mind building an app
 
 
 
 
 ## If you plan to collaborate with one or two classmates, what are their names?
+I plan on working solo
 
 
 
 #Do you have any questions of your own?
+Where would we code to build an app or game for iPhone?
+How would we publish it to the app store if we finish it?
